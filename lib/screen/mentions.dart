9965d15a3de0b24e1e92745346c2b502dc38ac98
Pablo-ED1 <a_pablo.aguilera.salinas@iespablopicasso.es>
2024-11-25 19:41:51 +0100

    Entrega 4

---
+++ lib/screen/mentions.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class Mentions extends StatelessWidget {
+  final List<String> images = [
+    'assets/images/image1.png',
+    'assets/images/image2.png',
+    'assets/images/image3.png',
+    'assets/images/image4.png',
+    'assets/images/image5.png',
+    'assets/images/image6.png',
+    'assets/images/image7.png',
+    'assets/images/image8.png',
+    'assets/images/image9.png',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+                child: Padding(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset(
+                      'assets/images/image1.png',
+                      fit: BoxFit.cover,
+                      height: 130,
+                    ))),
+            Expanded(
+                child: Padding(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset(
+                      'assets/images/image2.png',
+                      fit: BoxFit.cover,
+                      height: 130,
+                    ))),
+            Expanded(
+                child: Padding(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset(
+                      'assets/images/image3.png',
+                      fit: BoxFit.cover,
+                      height: 130,
+                    ))),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+                child: Padding(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset(
+                      'assets/images/image4.png',
+                      fit: BoxFit.cover,
+                      height: 130,
+                    ))),
+            Expanded(
+                child: Padding(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset(
+                      'assets/images/image5.png',
+                      fit: BoxFit.cover,
+                      height: 130,
+                    ))),
+            Expanded(
+                child: Padding(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset(
+                      'assets/images/image6.png',
+                      fit: BoxFit.cover,
+                      height: 130,
+                    ))),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+                child: Padding(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset(
+                      'assets/images/image7.png',
+                      fit: BoxFit.cover,
+                      height: 130,
+                    ))),
+            Expanded(
+                child: Padding(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset(
+                      'assets/images/image8.png',
+                      fit: BoxFit.cover,
+                      height: 130,
+                    ))),
+            Expanded(
+                child: Padding(
+                    padding: const EdgeInsets.all(2),
+                    child: Image.asset(
+                      'assets/images/image9.png',
+                      fit: BoxFit.cover,
+                      height: 130,
+                    ))),
+          ],
+        )
+      ],
+    );
+  }
+}
--- lib/screen/stories.dart
+++ lib/screen/stories.dart
@@ -6,87 +6,125 @@ class Stories extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            //Me
-            Column(children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: SizedBox(
-                  width: 65,
-                  height: 65,
-                  child: Image.asset('assets/images/me.jpg', fit: BoxFit.cover),
-                ),
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              //Me
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: SizedBox(
+                      width: 65,
+                      height: 65,
+                      child: Image.asset('assets/images/me.jpg',
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                  const Text(
+                    'Me',
+                    style: TextStyle(
+                      fontFamily: 'ComicSans',
+                      fontSize: 14,
+                    ),
+                  ),
+                ]),
               ),
-              const Text(
-                'Me',
-                style: TextStyle(
-                  fontFamily: 'ComicSans',
-                  fontSize: 14,
-                ),
-              ),
-            ]),
 
-            //Travels
-            Column(children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: SizedBox(
-                  width: 65,
-                  height: 65,
-                  child: Image.asset('assets/images/travels.jpg',
-                      fit: BoxFit.cover),
-                ),
+              //Travels
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: SizedBox(
+                      width: 65,
+                      height: 65,
+                      child: Image.asset('assets/images/travels.jpg',
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                  const Text(
+                    'Travels',
+                    style: TextStyle(
+                      fontFamily: 'ComicSans',
+                      fontSize: 14,
+                    ),
+                  ),
+                ]),
               ),
-              const Text(
-                'Travels',
-                style: TextStyle(
-                  fontFamily: 'ComicSans',
-                  fontSize: 14,
-                ),
-              ),
-            ]),
 
-            //Friends
-            Column(children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: SizedBox(
-                  width: 65,
-                  height: 65,
-                  child: Image.asset('assets/images/friends.jpg',
-                      fit: BoxFit.cover),
-                ),
+              //Friends
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: SizedBox(
+                      width: 65,
+                      height: 65,
+                      child: Image.asset('assets/images/friends.jpg',
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                  const Text(
+                    'Friends',
+                    style: TextStyle(
+                      fontFamily: 'ComicSans',
+                      fontSize: 14,
+                    ),
+                  ),
+                ]),
               ),
-              const Text(
-                'Friends',
-                style: TextStyle(
-                  fontFamily: 'ComicSans',
-                  fontSize: 14,
-                ),
+
+              //Malaga
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: SizedBox(
+                      width: 65,
+                      height: 65,
+                      child: Image.asset('assets/images/malaga.jpg',
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                  const Text(
+                    'Málaga',
+                    style: TextStyle(
+                      fontFamily: 'ComicSans',
+                      fontSize: 14,
+                    ),
+                  ),
+                ]),
               ),
-            ]),
-            //Malaga
-            Column(children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(100),
-                child: SizedBox(
-                  width: 65,
-                  height: 65,
-                  child: Image.asset('assets/images/malaga.jpg',
-                      fit: BoxFit.cover),
-                ),
+
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: SizedBox(
+                      width: 65,
+                      height: 65,
+                      child: Image.asset('assets/images/catan.png',
+                          fit: BoxFit.cover),
+                    ),
+                  ),
+                  const Text(
+                    'CATAN',
+                    style: TextStyle(
+                      fontFamily: 'ComicSans',
+                      fontSize: 14,
+                    ),
+                  ),
+                ]),
               ),
-              const Text(
-                'Málaga',
-                style: TextStyle(
-                  fontFamily: 'ComicSans',
-                  fontSize: 14,
-                ),
-              ),
-            ]),
-          ],
+            ],
+          ),
         ),
         const SizedBox(height: 10),
         const Row(
